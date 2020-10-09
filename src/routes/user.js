fe6a4584071bba5477a5267a44496978be4f760e
Dymytrii Tretiakov <tretyakov3651@gmail.com>
2020-10-09 09:57:51 +0300

    Added get all users endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const withJWT = require('../utils/withJWT');
-const { approveUser, deleteUser } = require('../controllers/user');
+const { approveUser, deleteUser, getAllUsers } = require('../controllers/user');
 
 const router = express.Router();
+
+router.get('/', withJWT, getAllUsers);
 
 router.patch('/approve/:id', withJWT, approveUser);
 
